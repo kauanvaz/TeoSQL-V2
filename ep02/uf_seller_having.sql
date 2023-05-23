13541438136724a6297828041601e7ff3d2fe272
Kauan Vaz <kauanvazc@gmail.com>
2023-05-22 21:57:02 -0300

    Adição de comentários explicativos

--- ep02/uf_seller_having.sql
+++ ep02/uf_seller_having.sql
@@ -3,11 +3,14 @@ select seller_state,
 
 from tb_sellers
 
--- filtro pré agg (agregação)
+-- filtro pré agregação)
+-- WHERE funciona com operações em cima das colunas da tabela original (com dados já existentes)
+-- O COUNT acontece depois do GROUP BY, ou seja, o WHERE não funcionaria para um filtro em cima de uma coluna resultante do COUNT
 where seller_state in ('SP', 'RJ', 'PR', 'AC')
 
 -- agregação
 group by seller_state
 
 -- filtro pós agregação
+-- HAVING é usado para operações com dados existentes após o GROUP BY, como é o caso do COUNT(*) nesse exemplo
 having count(*) > 10 -- remove estados com menos de 10 sellers
