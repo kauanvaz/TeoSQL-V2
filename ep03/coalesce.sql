4742a26f7b2ad2eb36b5cb8efa1f7a1917a071d0
Kauan Vaz <kauanvazc@gmail.com>
2023-05-23 20:55:56 -0300

    Adição de comentários explicativos

--- ep03/coalesce.sql
+++ ep03/coalesce.sql
@@ -1,4 +1,4 @@
 select distinct
            coalesce(product_category_name, 'outros') as categoria_fillna
-
+           -- coalesce(product_category_name, 'outros', 'outros2', 'outros3'). O comando usa o primeiro valor que não for nulo 
 from tb_products
